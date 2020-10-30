8e53cf5202ec640de856db27997aac341b28b959
MARCELO073 <marcelonogueira072@gmail.com>
2020-10-30 14:35:03 -0300

    inplementei as clases do projeto

---
+++ PIMVIII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIMVIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
